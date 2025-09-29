3be2f90021154e4a48df74bd9a3591d6a21fba0d
AmriShanab <amrishanab@gmail.com>
2025-09-29 13:20:05 +0530

    new update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/',
+    // Add this line to force the asset base path to the web root
+    base: '/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
